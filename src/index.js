c78a8518fda1072470ba4776909ceef48ab1a180
Olumide <olumidedaramola426@gmail.com>
2024-07-30 18:51:55 +0100

    feat: added graphql queries

--- src/index.js
+++ src/index.js
@@ -4,16 +4,43 @@ require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
+
+let notes = [
+   {id: '1', content: 'This is a note', author: 'Adam Scott'},
+   {id: '2', content: 'This is another note', author: 'Harlow Everly'},
+   {id: '3', content: 'Oh look, another note', author: 'Riley Harrison'}
+]
+
 // define the schema
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]! 
+    note(id: ID): Note! 
+  },
+
+  type Pizza {
+  id: ID!
+  size: String!
+  slices: Int!
+  toppings: [String]
+  },
+
+  type Note {
+  id: ID!
+  content: String!
+  author: String!
   }
 `;
 // define the resolvers
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
+    notes: () => notes,
+    // Only return the information requested by the user
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id)
+    }
   },
 };
 
